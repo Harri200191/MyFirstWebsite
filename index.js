525b9a5a3217db787d89ef7620626452e8807156
haris rehman <harisrehmanchugtai@gmail.com>
2023-08-29 22:26:12 +0500

    Update

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const express = require("express");
 const path = require("path");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const appi = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 const PublicPath = path.join(__dirname, "public");
 const app = express();
