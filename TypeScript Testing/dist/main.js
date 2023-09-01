14496cc75ec798c864c1826bd5351a49f53d6973
haris rehman <harisrehmanchugtai@gmail.com>
2023-09-02 00:39:15 +0500

    Update

---
+++ TypeScript Testing/dist/main.js
@@ -0,0 +1,4 @@
+"use strict";
+console.log("This is just a testout line here!");
+let age = 20;
+console.log("The age is: ", age.toString());
---
+++ TypeScript Testing/src/main.ts
@@ -0,0 +1,5 @@
+console.log("This is just a testout line here!");
+
+let age: number = 20;
+
+console.log("The age is: ", age.toString());
