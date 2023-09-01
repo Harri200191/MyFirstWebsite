fa9e1806fd6ff39a46adbe30a56fd011006042f6
haris rehman <harisrehmanchugtai@gmail.com>
2023-09-01 22:49:18 +0500

    Update

--- mongodb.js
+++ mongodb.js
@@ -1,1 +1,9 @@
-// Currently empty
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  // Add more fields as needed
+});
+
+module.exports = mongoose.model('User', userSchema);
