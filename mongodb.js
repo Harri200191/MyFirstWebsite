d054c1ecdea67cbbfae5a53a59ac9a953ba393b1
haris rehman <harisrehmanchugtai@gmail.com>
2023-09-05 17:32:54 +0500

    Update

--- mongodb.js
+++ mongodb.js
@@ -1,5 +1,5 @@
-/* const mongoose = require('mongoose');
-const mongoURI = 'mongodb://localhost:27017';
+const mongoose = require('mongoose');
+const mongoURI = 'mongodb://0.0.0.0:27017';
 
 mongoose.connect(mongoURI, {
   useNewUrlParser: true,
@@ -11,24 +11,6 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => {
   console.log('Connected to MongoDB!');
-}); */
-
-// ATTEMPT 2 -------------------------------------------------------------
-const { MongoClient } = require('mongodb');
-const url = 'mongodb://0.0.0.0:27017';
-const dbName = 'test';
-
-async function connectToMongoDB() {
-  try {
-    const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
-    await client.connect();
-    const db = client.db(dbName);
-    client.close();
-  } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
-  }
-}
-
-connectToMongoDB();
+});
 
 
