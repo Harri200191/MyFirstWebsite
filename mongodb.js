4e37cb54156a1b7a07191dd4c816cf1e32945d63
haris rehman <harisrehmanchugtai@gmail.com>
2023-09-05 17:26:18 +0500

    Update

--- mongodb.js
+++ mongodb.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+/* const mongoose = require('mongoose');
 const mongoURI = 'mongodb://localhost:27017';
 
 mongoose.connect(mongoURI, {
@@ -11,7 +11,7 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => {
   console.log('Connected to MongoDB!');
-});
+}); */
 
 // ATTEMPT 2 -------------------------------------------------------------
 const { MongoClient } = require('mongodb');
@@ -28,4 +28,21 @@ async function connectToMongoDB() {
     console.error('Error connecting to MongoDB:', error);
   }
 }
+
+async function insertDocument() {
+  try {
+    const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+    await client.connect();
+    const db = client.db(dbName);
+    const collection = db.collection('num1');
+    const result = await collection.insertOne({ name: 'John Doe', age: 30 });
+    console.log('Document inserted:', result.ops);
+    client.close();
+  } catch (error) {
+    console.error('Error inserting document:', error);
+  }
+}
+
+insertDocument();
+
 
