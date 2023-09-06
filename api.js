70b4e132a78954a6e8dc4ce5182256ff3e916556
haris rehman <harisrehmanchugtai@gmail.com>
2023-09-07 00:21:20 +0500

    Update

--- api.js
+++ api.js
@@ -5,18 +5,24 @@ const app = express();
 
 app.use(express.json());;
 
+// To send response to postman
 app.get('/', async (req, resp) =>{
     let data = await dbConnect();
     data = await data.find({}).toArray();
     resp.send(data);
 });
 
+// You get data from postman POST API and that sent data is received using req.body
 app.post('/', async (req, resp) => {
     let data = await dbConnect();
-    data = await data.find({}).toArray();
-    resp.send(data);
+    let result = await data.insert(req.body);
+    resp.send(result);
+});
 
-    console.log(req.body);
+app.put('/', async (req, resp) =>{
+    let data = await dbConnect();
+    let result = await data.updateOne(req.body);
+    resp.send(result);
 });
 
 app.listen(3000);
