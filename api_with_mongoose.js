a0d5f08db4de8ee56c73b41ef52e9c3c543f64e8
haris rehman <harisrehmanchugtai@gmail.com>
2023-09-07 01:31:01 +0500

    Update

--- api_with_mongoose.js
+++ api_with_mongoose.js
@@ -5,10 +5,28 @@ const schemas_and_models = require("./DataBase_Mongoose/schemas_and_models");
 const app = express();
 app.use(express.json());;
 
-app.post('/', async (req, resp) => {
+app.post('/create', async (req, resp) => {
     let data = new schemas_and_models(req.body);
     let result = await data.save();
     resp.send(result);
+});
+
+app.get('/list', async (req, resp) => {
+    let data = await schemas_and_models.find();
+    resp.send(data);
+});
+
+app.delete('/delete/:_id', async (req, resp) => {
+    let data = await schemas_and_models.deleteOne(req.params);
+    resp.send(data);
+});
+
+app.put('/update/:_id', async (req, resp) => {
+    let data = await schemas_and_models.updateOne(
+        req.params,
+        { $set: req.body }
+    );
+    resp.send(data);
 });
 
 app.listen(5000);
