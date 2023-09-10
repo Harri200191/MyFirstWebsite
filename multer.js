82ab3525e0f45fa5c0b265061ff7a2057c4c27b6
haris rehman <harisrehmanchugtai@gmail.com>
2023-09-10 23:40:30 +0500

    Update

--- multer.js
+++ multer.js
@@ -5,6 +5,20 @@ const schemas_and_models = require("./DataBase_Mongoose/schemas_and_models");
 const app = express();
 app.use(express.json());
 
+const upload = multer({
+    storage: multer.diskStorage({
+        destination: function(req, file, cb){
+            cb(null, "uploads")
+        },
 
+        filename: function(req, file, cb){
+            cb(null, file.fieldname+"-"+Date.now()+".jpg")
+        }
+    })
+}).single("test_file");
+
+app.post("/upload", upload, (req, resp) => {
+    resp.send("Uploaded file!");
+});
 
 app.listen(4000);
