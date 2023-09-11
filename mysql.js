509c553a88f81825b0ca65714542e5f20851746b
haris rehman <harisrehmanchugtai@gmail.com>
2023-09-11 19:25:48 +0500

    Update

---
+++ mysql.js
@@ -0,0 +1,21 @@
+const mysql = require("mysql"); 
+
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "test"
+});
+
+con.connect((err) => {
+    if (err){
+        console.log("Error");
+    }
+    else{
+        console.warn("Connected");
+    }
+});
+
+con.query("select * from users", (err, result) =>{
+    console.warn(result);
+});
